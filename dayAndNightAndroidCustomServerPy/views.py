16a8ba1e35c189673a89239cfeb19c32cddab3ce
1030907690 <1030907690@qq.com>
2019-12-09 23:04:52 +0800

    拼接数据

--- dayAndNightAndroidCustomServerPy/views.py
+++ dayAndNightAndroidCustomServerPy/views.py
@@ -39,3 +39,7 @@ def videoDetail(request):
     return HttpResponse("");
 
 
+if __name__ == '__main__':
+    print()
+    json_str = '{"videoGroupList":[{"group":"m3u8","videoList":[{"name":"第1集","url":"http://xxxx.m3u8"}]}],"url":"http://wolongzy.net/detail/299773.html","performer":"胡歌","imageUrl":"http://cn2.3days.cc/1575873305179637.jpeg","name":"南方车站的聚会[TS]","plot":"南方车站的聚会"}'
+    print(json_str)
